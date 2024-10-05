a2c5b7ce472419696b05a406556fa51c1a1a43f0
LemonkaPix <maciejwrobel029@gmail.com>
2024-10-05 17:04:00 +0200

    czcionka

--- Assets/_Game/Ui/UI_Scripts/Journal.cs
+++ Assets/_Game/Ui/UI_Scripts/Journal.cs
@@ -7,18 +7,19 @@ public class Journal : MonoBehaviour
 {
     [SerializeField] GameObject journal;
     [SerializeField] CanvasGroup uiElement;
+    [SerializeField] private float journalSpeed = 0.25f;
     public bool isOpen = false;
 
     
     public void JournalOpen()
     {
         journal.SetActive(true);
-        LeanTween.alphaCanvas(uiElement, 1f, 0.5f).setEase(LeanTweenType.linear);
+        LeanTween.alphaCanvas(uiElement, 1f, journalSpeed).setEase(LeanTweenType.linear);
     }
 
     public void JournalClose()
     {
-        LeanTween.alphaCanvas(uiElement, 0f, 0.5f).setEase(LeanTweenType.linear).setOnComplete(() => journal.SetActive(false)); 
+        LeanTween.alphaCanvas(uiElement, 0f, journalSpeed).setEase(LeanTweenType.linear).setOnComplete(() => journal.SetActive(false)); 
 
     }
 
