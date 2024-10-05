6314599988320bf019b13290e38f5414bf44dd07
LemonkaPix <maciejwrobel029@gmail.com>
2024-10-06 01:53:57 +0200

    save commit

--- Assets/_Game/Ui/UI_Scripts/Journal.cs
+++ Assets/_Game/Ui/UI_Scripts/Journal.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Journal : MonoBehaviour
 {
     [SerializeField] GameObject journal;
-    [SerializeField] GameObject[] journalContent;
+
+    [FormerlySerializedAs("journalContent")] [SerializeField]
+    GameObject planetPanel;
+    [SerializeField] GameObject NotePanel;
+
     [SerializeField] CanvasGroup uiElement;
     [SerializeField] private float journalSpeed = 0.25f;
 
-    [Header("Confirmation Panel")]
-    [SerializeField] TMP_Text exoPlanetName;
+    [Header("Confirmation Panel")] [SerializeField]
+    TMP_Text exoPlanetName;
+
     [SerializeField] TMP_Text exoPlanetDescription;
     [SerializeField] GameObject exoPlanetComposition;
     [SerializeField] private Transform ImagesParent;
     [SerializeField] List<GameObject> tempImages = new List<GameObject>();
 
-    [Header("Planet Choosing Panel")]
-    [SerializeField] TMP_Text exoPlanetNameConfirm;
+    [SerializeField] private GameObject sketchpanel;
+
+    [Header("Planet Choosing Panel")] [SerializeField]
+    TMP_Text exoPlanetNameConfirm;
+
     [SerializeField] TMP_Text exoPlanetLookDesc;
 
     [SerializeField] Image[] confirmImages = new Image[3];
@@ -34,57 +43,67 @@ public class Journal : MonoBehaviour
 
         foreach (var planet in dh.planets)
         {
+            bool test = true;
             foreach (var element in planet.elements)
             {
-                //
+                if (!dh.chosenElements.Contains(element))
+                {
+                    test = false;
+                }
+            }
+
+            if (test)
+            {
+                findedPlanet = planet;
+                break;
             }
         }
 
-        if(findedPlanet == null)
+        if (findedPlanet == null)
         {
-            foreach (GameObject item in journalContent)
-            {
-                item.SetActive(false);
-            }
+            planetPanel.SetActive(false);
+
             // return;
-        } else
+        }
+        else
         {
-            foreach (GameObject item in journalContent)
-            {
-                item.SetActive(true);
-            }
+            planetPanel.SetActive(true);
+
             exoPlanetName.text = findedPlanet.name;
             exoPlanetDescription.text = findedPlanet.PlanetDescription;
-            
+
             exoPlanetNameConfirm.text = findedPlanet.name;
             exoPlanetLookDesc.text = findedPlanet.PlanetLookDescription;
         }
 
 
-        
         print("before");
-        
-        
-        foreach(GameObject gameObject in tempImages)
+
+
+        foreach (GameObject gameObject in tempImages)
         {
             Destroy(gameObject);
         }
+
         tempImages.Clear();
 
-        
-        foreach(ElementObject element in dh.chosenElements)
+
+        foreach (ElementObject element in dh.chosenElements)
         {
             GameObject img = Instantiate(exoPlanetComposition, ImagesParent);
             img.GetComponent<Image>().sprite = element.Sprite;
             tempImages.Add(img);
         }
-
     }
 
-    private void sketchSelect(DataHandler dh)
+    public void sketchSelect()
     {
+        NotePanel.SetActive(false);
+        planetPanel.SetActive(false);
+        var dh = DataHandler.instance;
+        sketchpanel.SetActive(true);
         List<PlanetObject> tempPlanets = dh.planets;
-        foreach(Image img in confirmImages)
+        foreach (Image img in confirmImages)
         {
             int randomIndex = Random.Range(0, tempPlanets.Count);
             img.sprite = dh.planets[randomIndex].PlanetSprite;
@@ -103,8 +122,7 @@ public class Journal : MonoBehaviour
 
     public void JournalClose()
     {
-        LeanTween.alphaCanvas(uiElement, 0f, journalSpeed).setEase(LeanTweenType.linear).setOnComplete(() => journal.SetActive(false)); 
-
+        LeanTween.alphaCanvas(uiElement, 0f, journalSpeed).setEase(LeanTweenType.linear)
+            .setOnComplete(() => journal.SetActive(false));
     }
-
 }
