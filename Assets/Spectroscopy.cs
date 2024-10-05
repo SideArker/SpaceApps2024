6b2d96bbbae592f44b404ad198a10af187d7bcac
LemonkaPix <maciejwrobel029@gmail.com>
2024-10-05 15:29:05 +0200

    dla radka do naprawy :)

--- Assets/Spectroscopy.cs
+++ Assets/Spectroscopy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Spectroscopy : MonoBehaviour
 {
