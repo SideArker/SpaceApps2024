a6d4f230d38aa481341025850cdd191c92d55035
SideArker <rsowa126@gmail.com>
2024-10-05 15:48:21 +0200

    render tex

--- Assets/_Game/Components/RandomRotation.cs
+++ Assets/_Game/Components/RandomRotation.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 
 public class RandomRotation : MonoBehaviour
 {
-    private void Start()
+    private void OnEnable()
     {
         Vector3 vct = new Vector3(0, 0, Random.Range(0,360));
         transform.Rotate(vct);
