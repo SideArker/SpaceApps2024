f5a510d3368f9090512a6c8b6fb0b1f3bba4f0db
awaveofreason <astroxnomy@gmail.com>
2024-10-06 10:56:32 +0200

    build err fix

--- Assets/_Game/Components/Editors/MainMenuEditor.cs
+++ Assets/_Game/Components/Editors/MainMenuEditor.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -58,3 +60,5 @@ public class MainMenuEditor : Editor
         base.DrawDefaultInspector();
     }
 }
+
+#endif
